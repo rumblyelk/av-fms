08deb9b7ca61d2839b01ed0f7bbc4aaf58a894a2
Betzalel Beck <betzalelbeck@gmail.com>
2022-11-20 22:02:28 +0200

    add role to user model

--- av-fms/vehicles.py
+++ av-fms/vehicles.py
@@ -16,4 +16,25 @@ def index():
     vehicles = db.execute(
         'SELECT * FROM vehicle'
     ).fetchall()
+    create_staff_user()
     return render_template('vehicles/index.html', vehicles=vehicles)
+
+
+def create_staff_user():
+    """
+    Since this whole system is a demo, this code is here to provide a staff user for testing.
+    After creating a normal user for the first time and loading the vehicles page, the ability
+    to log in with a staff role with the following credentials will be available:
+        username: staffUser
+        password: staffpass
+    """
+    db = get_db()
+    user = db.execute(
+        'SELECT * FROM user WHERE username = ?', ('staffUser',)
+    ).fetchone()
+    if user is None:
+        db.execute(
+            'INSERT INTO user (username, password, role) VALUES (?, ?, ?)',
+            ('staffUser', generate_password_hash('staffpass'), 'STAFF')
+        )
+        db.commit()
