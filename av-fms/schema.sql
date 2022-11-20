1bbf164a703f9be7d2484aabd5a89a6381c67082
Betzalel Beck <betzalelbeck@gmail.com>
2022-11-20 20:42:32 +0200

    create vehicle table

--- av-fms/schema.sql
+++ av-fms/schema.sql
@@ -2,5 +2,15 @@ DROP TABLE IF EXISTS user;
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    vehicle_id INTEGER -- the id of the vehicle that the user took
+);
+
+DROP TABLE IF EXISTS vehicle;
+CREATE TABLE vehicle (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    license_plate_number TEXT NOT NULL,
+    manufacturer TEXT NOT NULL,
+    user_id INTEGER, -- if available is false, this is the user who took the vehicle
+    available BOOLEAN NOT NULL
 );
